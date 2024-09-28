9ddce566713ececa3e28255d058abe8041b3e3a3
ingenarel <ingenarelitems@gmail.com>
2024-09-28 23:47:04 +0600

    new updatescript logic change test

--- updatescript.py
+++ updatescript.py
@@ -14,8 +14,12 @@ def git_project_latest_commit_hash_check(
     provider = provider.strip().lower()
     if provider == "github":
         return requests.get(f"https://api.github.com/repos/{name_and_repo}/commits").json()[0]["sha"]
-    elif provider == "gitlab":
+    elif provider == "gitlab" and project_id != None:
         return requests.get(f"https://gitlab.com/api/v4/projects/{project_id}/repository/commits").json()[0]["id"]
+    elif provider == "gitlab" and project_id == None:
+        raise Exception("provider was gitlab but a project_id was not provided")
+    else:
+        raise NotImplementedError(f"this function doesn't support {provider}")
 
 def git_project_latest_zip_hash_check(
         name_and_repo:str,
@@ -33,30 +37,27 @@ def git_project_latest_zip_hash_check(
     and you would also need to provide the package_name variable, which is the repo name.
     """
     if provider == "github":
-        git_project_latest_zip_hash_url = f"https://github.com/{name_and_repo}/archive/refs/heads/{branch}.zip"
-    elif provider == "gitlab":
-        git_project_latest_zip_hash_url = f"https://gitlab.com/{name_and_repo}/-/archive/{branch}/heads/{project_name}-{branch}.zip"
+        git_project_latest_zip_url = f"https://github.com/{name_and_repo}/archive/refs/heads/{branch}.zip"
+    elif provider == "gitlab" and project_name != None:
+        git_project_latest_zip_url = f"https://gitlab.com/{name_and_repo}/-/archive/{branch}/heads/{project_name}-{branch}.zip"
+    elif provider == "gitlab" and project_name == None:
+        raise Exception("provider was gitlab but a project_name was not provided")
     else:
-        raise NotImplementedError
+        raise NotImplementedError(f"this function doesn't support {provider}")
     
     # chatgpt written code starts here
     hash_obj = hashlib.new('sha256')
-    with requests.get(git_project_latest_zip_hash_url, stream=True) as response:
+    with requests.get(git_project_latest_zip_url, stream=True) as response:
         for chunk in response.iter_content(chunk_size=8192):
             if chunk:
                 hash_obj.update(chunk)
     return hash_obj.hexdigest()
     # chatgpt written code ends here
 
-def read_project_commit_hash(package_name:str) -> str:
+def read_project_commit_and_zip_hash(package_name:str) -> tuple:
     with open(f'bucket/{package_name}.json', 'r') as manifest:
         data = json.load(manifest)
-    return data["version"]
-
-def read_project_zip_hash(package_name:str) -> str:
-    with open(f'bucket/{package_name}.json', 'r') as manifest:
-        data = json.load(manifest)
-    return data["hash"]
+    return (data["version"], data["hash"])
 
 def write_project_commit_hash(package_name:str, commit_hash:str):
     with open(f'bucket/{package_name}.json', 'r') as manifest:
@@ -74,101 +75,110 @@ def write_project_zip_hash(package_name:str, zip_hash:str):
         manifest.write(json.dumps(data, indent=4))
     print(f"{package_name} zip hash changed to {zip_hash}")
 
-def main():
-    list_of_github_projects = (
-        {
-            "name_and_repo": "ayn2op/discordo",
-            "package_name": "discordo-git",
-            "branch": "main",
-        },
-        {
-            "name_and_repo": "jesseduffield/lazygit",
-            "package_name": "lazygit-git",
-            "branch": "master",
-        },
-        {
-            "name_and_repo": "ajeetdsouza/zoxide",
-            "package_name": "zoxide-git",
-            "branch": "main",
-        },
-        {
-            "name_and_repo": "junegunn/fzf",
-            "package_name": "fzf-git",
-            "branch": "master",
-        },
-        {
-            "name_and_repo": "cli/cli",
-            "package_name": "gh-git",
-            "branch": "trunk",
-        },
-        {
-            "name_and_repo": "BurntSushi/ripgrep",
-            "package_name": "ripgrep-git",
-            "branch": "master",
-        },
-    )
+def git_project_check_and_fix(list_of_git_projects:dict) -> None:
+    for provider in list_of_git_projects:
+        for projectdata in list_of_git_projects[provider]:
+            if provider == "github":
+                git_project = projectdata["name_and_repo"]
+                project_name = None
+            elif provider == "gitlab":
+                git_project == projectdata["projectid"]
+                project_name = projectdata["project_name"]
+            else:
+                raise NotImplementedError(f"given a git provider \"{provider}\" that isn't supported")
+            package = projectdata["package_name"]
+            branch = projectdata["branch"]
 
-    for projectdata in list_of_github_projects:
-        project = projectdata["name_and_repo"]
-        package = projectdata["package_name"]
-        branch = projectdata["branch"]
-
-        last_commit_hash = git_project_latest_commit_hash_check(
-            name_and_repo=project,
-            provider="github",
-        )
-        manifest_commit_hash = read_project_commit_hash(package)
-        if last_commit_hash != manifest_commit_hash:
-            write_project_commit_hash(package, last_commit_hash)
-
-            last_zip_hash = git_project_latest_zip_hash_check(
-                name_and_repo=project,
-                branch=branch,
+            last_commit_hash = git_project_latest_commit_hash_check(
+                name_and_repo=git_project,
                 provider="github",
             )
-            # manifest_zip_hash = read_project_zip_hash(package)
-        # if last_zip_hash != manifest_zip_hash:
-            write_project_zip_hash(package, last_zip_hash)
+            manifest_commit_hash, manifest_zip_hash = read_project_commit_and_zip_hash(package)
+            if last_commit_hash != manifest_commit_hash:
+                write_project_commit_hash(package, last_commit_hash)
 
-    list_of_gitlab_projects = (
-        {
-            "projectid":"20825969",
-            "package_name": "sheepit-git",
-            "branch":"master",
-            "project_name": "client",
-        },
-        {
-            "projectid":"34675721",
-            "package_name": "glab-git",
-            "branch":"main",
-            "project_name": "cli",
-        },
-    )
+                last_zip_hash = git_project_latest_zip_hash_check(
+                    name_and_repo=git_project,
+                    branch=branch,
+                    provider="github",
+                    project_name=project_name
+                )
+                if last_zip_hash != manifest_zip_hash:
+                    write_project_zip_hash(package, last_zip_hash)
 
-    for projectdata in list_of_gitlab_projects:
-        projectid = projectdata["projectid"]
-        package = projectdata["package_name"]
-        branch = projectdata["branch"]
-        project_name = projectdata["project_name"]
-
-        last_commit_hash = git_project_latest_commit_hash_check(
-            name_and_repo=projectid,
-            provider="gitlab",
-            project_id=projectid,
-        )
-        manifest_commit_hash = read_project_commit_hash(package)
-        if last_commit_hash != manifest_commit_hash:
-            write_project_commit_hash(package, last_commit_hash)
-
-            last_zip_hash = git_project_latest_zip_hash_check(
-                name_and_repo=projectid,
-                branch=branch,
-                provider="gitlab",
-                project_name=project_name,
-            )
-            # manifest_zip_hash = read_project_zip_hash(package)
-        # if last_zip_hash != manifest_zip_hash:
-            write_project_zip_hash(package, last_zip_hash)
-
+def main():
+    list_of_git_projects = {
+        "github": (
+            {
+                "name_and_repo": "ayn2op/discordo",
+                "package_name": "discordo-git",
+                "branch": "main",
+            },
+            {
+                "name_and_repo": "jesseduffield/lazygit",
+                "package_name": "lazygit-git",
+                "branch": "master",
+            },
+            {
+                "name_and_repo": "ajeetdsouza/zoxide",
+                "package_name": "zoxide-git",
+                "branch": "main",
+            },
+            {
+                "name_and_repo": "junegunn/fzf",
+                "package_name": "fzf-git",
+                "branch": "master",
+            },
+            {
+                "name_and_repo": "cli/cli",
+                "package_name": "gh-git",
+                "branch": "trunk",
+            },
+            {
+                "name_and_repo": "BurntSushi/ripgrep",
+                "package_name": "ripgrep-git",
+                "branch": "master",
+            },
+        ),
+        "gitlab": (
+            {
+                "projectid":"20825969",
+                "package_name": "sheepit-git",
+                "branch":"master",
+                "project_name": "client",
+            },
+            {
+                "projectid":"34675721",
+                "package_name": "glab-git",
+                "branch":"main",
+                "project_name": "cli",
+            },
+        ),
+    }
+    git_project_check_and_fix(list_of_git_projects=list_of_git_projects)
+    # for projectdata in list_of_git_projects["gitlab"]:
+    #     projectid = projectdata["projectid"]
+    #     package = projectdata["package_name"]
+    #     branch = projectdata["branch"]
+    #     project_name = projectdata["project_name"]
+    #
+    #     last_commit_hash = git_project_latest_commit_hash_check(
+    #         name_and_repo=projectid,
+    #         provider="gitlab",
+    #         project_id=projectid,
+    #     )
+    #     manifest_commit_hash, manifest_zip_hash = read_project_commit_and_zip_hash(package)
+    #     if last_commit_hash != manifest_commit_hash:
+    #         write_project_commit_hash(package, last_commit_hash)
+    #
+    #         last_zip_hash = git_project_latest_zip_hash_check(
+    #             name_and_repo=projectid,
+    #             branch=branch,
+    #             provider="gitlab",
+    #             project_name=project_name,
+    #         )
+    #         if last_zip_hash != manifest_zip_hash:
+    #             write_project_zip_hash(package, last_zip_hash)
+    #
 if __name__ == "__main__":
     main()
